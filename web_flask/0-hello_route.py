d458aa7a35e8229e8cdf3d157db9342692cee913
blackdreamer15 <bakijessy@gmail.com>
2024-07-25 02:58:23 +0000

    fix: Add fixes in accordance to PEP8

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -6,10 +6,11 @@ from flask import Flask
 
 app = Flask(__name__)
 
+
 @app.route("/", strict_slashes=False)
 def root():
-  """Returns the string: Hello HBNB!"""
-  return "Hello HBNB!"
+    """Returns the string: Hello HBNB!"""
+    return "Hello HBNB!"
 
 if __name__ == "__name__":
-  app.run(host='0.0.0.0', port=5000)
+    app.run(host='0.0.0.0', port=5000)
