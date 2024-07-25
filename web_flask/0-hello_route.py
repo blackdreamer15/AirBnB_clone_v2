f17f70903057ded60fc582465ab9ce56d1e45905
blackdreamer15 <bakijessy@gmail.com>
2024-07-25 02:46:06 +0000

    chore: Add host and port on which the app runs

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -4,7 +4,8 @@ app = Flask(__name__)
 
 @app.route("/", strict_slashes=False)
 def root():
+  """Returns the string: Hello HBNB!"""
   return "Hello HBNB!"
 
 if __name__ == "__name__":
-  app.run()
+  app.run(host='0.0.0.0', port=5000)
