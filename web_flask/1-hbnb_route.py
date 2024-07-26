54e00ca33705aac34f4e9a7519d4aa4848fc9184
blackdreamer15 <bakijessy@gmail.com>
2024-07-26 03:18:21 +0000

    chore: Add  `/hbnb` route that returns the string `HBNB`

--- web_flask/1-hbnb_route.py
+++ web_flask/1-hbnb_route.py
@@ -12,5 +12,11 @@ def root():
     """Returns Hello HBNB!"""
     return "Hello HBNB!"
 
+
+@app.route("/hbnb", strict_slashes=False)
+def hbnb():
+    """Returns HBNB"""
+    return "HBNB"
+
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=5000)
