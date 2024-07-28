890b7831dee0025c0d1cebaf66044eb26be22e39
blackdreamer15 <bakijessy@gmail.com>
2024-07-28 05:46:09 +0000

    chore: Add route to render a HTML page with states in alphabetical order

--- web_flask/7-states_list.py
+++ web_flask/7-states_list.py
@@ -1,54 +1,25 @@
 #!/usr/bin/python3
 """
-Initialization of Flask app
+Start Flask app
 """
-from flask import Flask, render_template
 
+from flask import Flask, render_template
+from models import *
+from models import storage
 app = Flask(__name__)
 
 
-@app.route("/", strict_slashes=False)
-def root():
-    """Returns Hello HBNB!"""
-    return "Hello HBNB!"
+@app.route('/states_list', strict_slashes=False)
+def states_list():
+    """display a HTML page with the states listed in alphabetical order"""
+    states = sorted(list(storage.all("State").values()), key=lambda x: x.name)
+    return render_template('7-states_list.html', states=states)
 
 
-@app.route("/hbnb", strict_slashes=False)
-def hbnb():
-    """Returns HBNB"""
-    return "HBNB"
+@app.teardown_appcontext
+def teardown_db(exception):
+    """closes the storage on teardown"""
+    storage.close()
 
-
-@app.route("/c/<text>", strict_slashes=False)
-def cisfun(text):
-    """Returns "C" followed by the value of the text variable"""
-    return f"C {text.replace('_', ' ')}"
-
-
-@app.route("/python/", strict_slashes=False)
-@app.route("/python/<text>", strict_slashes=False)
-def python_is_cool(text="is_cool"):
-    """Returns "Python" followed by the value of the text variable"""
-    return f"Python {text.replace('_', ' ')}"
-
-
-@app.route("/number/<int:n>", strict_slashes=False)
-def is_n_a_number(n):
-    """Returns "n is a number" only if n is an integer"""
-    return f"{n:d} is a number"
-
-
-@app.route("/number_template/<int:n>", strict_slashes=False)
-def number_template(n):
-    """Returns a HTML page only if n is an integer"""
-    return render_template("5-number.html", n=n)
-
-
-@app.route("/number_odd_or_even/<int:n>", strict_slashes=False)
-def number_odd_or_even(n):
-    """Returns a HTML page only if n is an integer"""
-    is_even = n % 2 == 0
-    return render_template("6-number_odd_or_even.html", n=n, is_even=is_even)
-
-if __name__ == "__main__":
-    app.run(host='0.0.0.0', port=5000)
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port='5000')
