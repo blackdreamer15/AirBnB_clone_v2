b4cb499eff17d7092b560c7603e1bd9c49f16138
blackdreamer15 <bakijessy@gmail.com>
2024-07-29 06:18:59 +0000

    fix: Add fixes to User model

--- models/user.py
+++ models/user.py
@@ -1,20 +1,20 @@
 #!/usr/bin/python3
-""" holds class User"""
-import models
+"""This module defines a class User"""
+import os
+from hashlib import md5
 from models.base_model import BaseModel, Base
-from os import getenv
 from sqlalchemy import Column, String
 from sqlalchemy.orm import relationship
 
 
 class User(BaseModel, Base):
-    """Representation of a user """
-    if models.storage_t == 'db':
-        __tablename__ = 'users'
+    """Definition of user class"""
+    if os.getenv('HBNB_TYPE_STORAGE') == 'db':
+        __tablename__ = "users"
         email = Column(String(128), nullable=False)
         password = Column(String(128), nullable=False)
-        first_name = Column(String(128), nullable=True)
-        last_name = Column(String(128), nullable=True)
+        first_name = Column(String(128))
+        last_name = Column(String(128))
         places = relationship("Place", backref="user")
         reviews = relationship("Review", backref="user")
     else:
@@ -22,7 +22,17 @@ class User(BaseModel, Base):
         password = ""
         first_name = ""
         last_name = ""
+        places = ""
+        reviews = ""
 
     def __init__(self, *args, **kwargs):
         """initializes user"""
+        if "password" in kwargs.keys():
+            kwargs["password"] = md5(kwargs["password"].encode()).hexdigest()
         super().__init__(*args, **kwargs)
+
+    def __setattr__(self, name, value):
+        """sets a password with md5 encryption"""
+        if name == "password":
+            value = md5(value.encode()).hexdigest()
+        super().__setattr__(name, value)
