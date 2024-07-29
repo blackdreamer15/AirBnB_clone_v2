cb87f2baf5eaa9322065d528cbc905dab2c1683c
blackdreamer15 <bakijessy@gmail.com>
2024-07-29 06:18:22 +0000

    fix: Add fixes to City model

--- models/city.py
+++ models/city.py
@@ -1,19 +1,20 @@
-#!/usr/bin/python
-""" holds class City"""
-import models
+#!/usr/bin/python3
+""" City Module for HBNB project """
+import os
 from models.base_model import BaseModel, Base
-from os import getenv
 from sqlalchemy import Column, String, ForeignKey
 from sqlalchemy.orm import relationship
 
 
 class City(BaseModel, Base):
-    """Representation of city """
-    if models.storage_t == "db":
+    """Definition of city class """
+    if os.getenv('HBNB_TYPE_STORAGE') == 'db':
         __tablename__ = 'cities'
-        state_id = Column(String(60), ForeignKey('states.id'), nullable=False)
         name = Column(String(128), nullable=False)
-        places = relationship("Place", backref="cities")
+        state_id = Column(String(60), ForeignKey('states.id'), nullable=False)
+        places = relationship("Place",
+                              backref="cities",
+                              cascade="delete")
     else:
         state_id = ""
         name = ""
